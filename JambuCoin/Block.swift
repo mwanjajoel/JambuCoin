09e9d183fad4d0f3e65a02a88274d3f80f409753
mwanjajoel <joelsilverworks@gmail.com>
2018-08-06 19:00:36 +0300

    Added logic to the app

--- JambuCoin/Block.swift
+++ JambuCoin/Block.swift
@@ -2,6 +2,17 @@
 import UIKit
 
 class Block {
-    // Enter the logic for the Block here
+    // Properties of the Block Class
+    
+    var hash: String!
+    var data: String!
+    var previousHash: String!
+    var index: Int!
+    
+    // this function generates a hash comprised of alphanumeric characters.
+    func generateHash() -> String {
+        
+        return NSUUID().uuidString.replacingOccurrences(of: "-", with: "")
+    }
     
 }
--- JambuCoin/Blockchain.swift
+++ JambuCoin/Blockchain.swift
@@ -3,5 +3,53 @@
 import UIKit
 
 class Blockchain {
-    // Enter the logic for the Blockchain here
+    
+    // create a chain comprising of Blocks.
+    var chain = [Block]()
+    
+    // this function creates a genesis block in the blockchain
+    func createGenesisBlock(data: String) {
+        
+        let genesisBlock = Block()
+        
+        //generate the beginning hash
+        genesisBlock.hash = genesisBlock.generateHash()
+        
+        //store data in the block
+        genesisBlock.data = data
+        
+        //set the previous hash to zero
+        genesisBlock.previousHash = "0000"
+        
+        // and set the genesis Block to be the index
+        genesisBlock.index = 0
+        
+        //append the genesis block to the chain
+        chain.append(genesisBlock)
+        
+    }
+    
+    // this function creates a new block
+    func createBlock(data: String) {
+        
+        //create a new instance of the Block class
+        let newBlock = Block()
+        
+        // and generate a hash for it
+        newBlock.hash = newBlock.generateHash()
+        
+        // put some data to it
+        newBlock.data = data
+        
+        // get the previous Hash and add it to this block
+        newBlock.previousHash = chain[chain.count - 1].hash
+        
+        // begin counting from the index
+        newBlock.index = chain.count
+        
+        // then append to the chain, hoorrayyy!!
+        chain.append(newBlock)
+        
+    }
+    
 }
