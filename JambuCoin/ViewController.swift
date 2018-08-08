eaa4948321996e01f4c4df34fbf618368c857eff
mwanjajoel <joelsilverworks@gmail.com>
2018-08-09 00:04:54 +0300

    Added the transaction function

--- JambuCoin/ViewController.swift
+++ JambuCoin/ViewController.swift
@@ -1,5 +1,3 @@
-
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -9,9 +7,58 @@ class ViewController: UIViewController {
     @IBOutlet weak var redLabel: UILabel!
     @IBOutlet weak var blueLabel: UILabel!
     
+    // jambucoin accounts we shall be using to transact
+    let firstAccount = 1000
+    let secondAccount = 0200
+    
+    // unleash our blockchain
+    let jambuCoinChain = Blockchain()
+    
+    // reward given out when mining takes place
+    let reward = 100
+    
+    // master account from where the JambuCoins come from, only has 999000000 Jambucoins
+    var accounts: [String: Int] = ["0000": 999000000]
+    
+    // this is an alert that shall be seen when a transaction is more than 999000000 Jambucoins
+    let invalidTransactionAlert = UIAlertController(title: "Dang! Invalid Transaction", message: "Please check the details of your transaction, change them and try again", preferredStyle: .alert)
+    
+    // MARK: - This is the transaction function that enables transfer of money from sender to receiver
+    
+    func transaction(from: String, to: String, amount: Int, type: String) {
+        
+        //check if sending account is nil
+        if accounts[from] == nil {
+            
+            //alert the user
+            self.present(invalidTransactionAlert, animated: true, completion: nil)
+            
+            return
+        //check to see if sending amount is less than zero
+        } else if accounts[from]! - amount < 0 {
+            
+            // alert the user, again
+            self.present(invalidTransactionAlert, animated: true, completion: nil)
+            
+            return
+          
+            // otherwise do the needful
+        } else {
+            
+            // update the value of the receiving account
+            accounts.updateValue(accounts[from]! - amount, forKey: from)
+        }
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
